6a3de52e59809cb873b12d84205a2a0619e574ad
Arthur Palamarchuk <apalamarchuk@cogniance.com>
2018-07-20 13:42:07 +0300

    little changes

--- tests/test_home_task.py
+++ tests/test_home_task.py
@@ -28,7 +28,7 @@ class TestMeetsMainPage(FixtureMeetsMainPage):
 
     @classmethod
     def setup_class(cls):
-        cls.driver = webdriver.Firefox()
+        cls.driver = webdriver.Chrome()
         cls.mp(cls)
 
     def setup_method(self, method):
